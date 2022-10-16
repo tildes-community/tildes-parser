0f0b0449930bb7a98a231f549fb81dbce38cec28
Bauke <me@bauke.xyz>
2022-10-16 18:09:04 +0200

    Make tests use .parse instead of Html.

--- tests/group.rs
+++ tests/group.rs
@@ -1,11 +1,10 @@
 use std::fs::read_to_string;
 
-use {insta::assert_debug_snapshot, scraper::Html, tildes_parser::Group};
+use {insta::assert_debug_snapshot, tildes_parser::Group};
 
 #[test]
 fn test_group_parsing() {
   let html = read_to_string("tests/samples/group.html").unwrap();
-  let html = Html::parse_document(&html);
-  let group = Group::from_html(&html).unwrap();
+  let group = &html.parse::<Group>().unwrap();
   assert_debug_snapshot!(group);
 }
--- tests/group_list.rs
+++ tests/group_list.rs
@@ -1,11 +1,10 @@
 use std::fs::read_to_string;
 
-use {insta::assert_debug_snapshot, scraper::Html, tildes_parser::GroupList};
+use {insta::assert_debug_snapshot, tildes_parser::GroupList};
 
 #[test]
 fn test_group_list_parsing() {
   let html = read_to_string("tests/samples/group_list.html").unwrap();
-  let html = Html::parse_document(&html);
-  let group_list = GroupList::from_html(&html).unwrap();
+  let group_list = html.parse::<GroupList>().unwrap();
   assert_debug_snapshot!(group_list);
 }
